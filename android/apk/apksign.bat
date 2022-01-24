1b55a3dcb3563f3928113373e8c173b7eaa73965
duanhouhai <houhai.duan@mihoyo.com>
2022-01-24 18:22:10 +0800

    change keystore。

--- android/apk/apksign.bat
+++ android/apk/apksign.bat
@@ -1,1 +1,3 @@
-java -jar "%~dp0..\apksigner.jar" sign --ks "%~dp0debug.keystore" --ks-key-alias debug --ks-pass pass:android --key-pass pass:android --v1-signing-enabled true --v2-signing-enabled true %1
+"%~dp0..\zipalign.exe" -v 4 %1 %1.out.apk
+java -jar "%~dp0..\apksigner.jar" sign --ks "%~dp0debug.keystore" --ks-key-alias debug --ks-pass pass:android --key-pass pass:android --v1-signing-enabled true --v2-signing-enabled true %1.out.apk
+move %1.out.apk %1
